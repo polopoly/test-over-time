143a4888cb9c8462fa25d315455ccbca5d3cdcfe
Polopoly prodtest00 <tester@prodtest00.polopoly.com>
2012-03-16 10:26:05 +0100

    Added histogram to timeline

--- timeline.js
+++ timeline.js
@@ -1,5 +1,6 @@
 var tests = [];
 var cols = [];
+var buckets = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 var startTime = null;
 
 var zoomFactor = 2;
@@ -8,6 +9,7 @@ function drawTimeline(url, element, nameElement, zoom) {
     zoomFactor = zoom;
     tests = [];
     cols = [];
+    buckets = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     startTime = null;
     var div = $('#' + element);
     div.html('');
@@ -52,6 +54,7 @@ function drawTimeline(url, element, nameElement, zoom) {
                 alert('Failed on line ' + i + ': "' + line + '"\n' + err); 
             }
         }
+        drawHistogram();
     });
 }
 
@@ -111,9 +114,56 @@ function endTest(testName, duration, nameDiv, resultType) {
             div.mouseleave(function() { nameDiv.html(''); });
             cols[i].append(div);
             tests[i] = null;
+
+            var bucket_idx = Math.round(Math.log(duration)/Math.log(2));
+            buckets[bucket_idx] = buckets[bucket_idx] + duration;
             return;
         }
     }
 
     alert('Test ' + testName + ' terminated without ever starting!');    
+}
+
+function drawHistogram() {
+    var canvas = document.getElementById('histogram');
+    var height = canvas.getAttribute('height');
+    var width = canvas.getAttribute('width');
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+
+    var lineargradient = ctx.createLinearGradient(0,0,0,height);
+    lineargradient.addColorStop(0,'rgb(180, 190, 230)');
+    lineargradient.addColorStop(1,'rgb(230, 220, 213)');
+
+    ctx.save();
+    ctx.strokeStyle = "rgb(90, 110, 150)";
+    ctx.fillStyle = lineargradient;
+    
+    var maxheight = 0;
+    for (var i = 0; i < buckets.length; i++) {
+        if (buckets[i] > maxheight) {
+            maxheight = buckets[i];
+        }
+    }
+
+    for (var i = 0; i < buckets.length; i++) {
+        var accum = buckets[i];
+        
+        ctx.beginPath();
+        ctx.moveTo(i * 5, 0);
+        var barsize = accum/(maxheight/height);
+        ctx.fillRect (i*(width/buckets.length), height-barsize, width/buckets.length, barsize);
+    }
+
+    ctx.restore();
+
+    // Fix fillText compatibility
+    if (!ctx.fillText && ctx.mozDrawText) { ctx.fillText = function(textToDraw, x, y, maxWidth) { ctx.translate(x, y); ctx.mozTextStyle = ctx.font; ctx.mozDrawText(textToDraw); ctx.translate(-x, -y); } } 
+    
+
+    ctx.save();
+    ctx.font = "15px Times New Roman";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+    ctx.fillText("Accumulated test time, log2 x-axis", 5, 18);
+    ctx.restore();
 }
