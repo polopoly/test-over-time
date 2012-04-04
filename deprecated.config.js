4d90d93e626a31aa646f4a4afef8a81f068df90b
Polopoly prodtest00 <tester@prodtest00.polopoly.com>
2012-04-04 10:38:54 +0200

    Updated with RELENG-10-5-DR2 //stahl

--- deprecated.config.js
+++ deprecated.config.js
@@ -2,12 +2,12 @@
  * Configuration. These determine both which failing tests to list
  * and which branches and environments to show in the history for each failing test.
  */
-var branches = ["RELENG-10-3-DR4", "RELENG-10-3-DR5"];
+var branches = ["RELENG-10-5-DR1", "RELENG-10-5-DR2"];
 var fetch_tickets = true;
 
 var environments = {
-    "RELENG-10-3-DR4": ["DEPRECATED-jboss+mysql", "DEPRECATED-jboss+tomcat+mysql"],
-    "RELENG-10-3-DR5": ["DEPRECATED-jboss+mysql", "DEPRECATED-jboss5+tomcat+mysql", "DEPRECATED-jboss+tomcat+mysql"]
+    "RELENG-10-5-DR1": ["DEPRECATED-jboss+mysql", "DEPRECATED-jboss5+tomcat+mysql", "DEPRECATED-jboss+tomcat+mysql"],
+    "RELENG-10-5-DR2": ["DEPRECATED-jboss+mysql", "DEPRECATED-jboss5+tomcat+mysql", "DEPRECATED-jboss+tomcat+mysql"]
 }
 
 var DAYS=30;
